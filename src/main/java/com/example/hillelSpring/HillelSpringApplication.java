cdf073246585ee0857bda1d9bea0b2bbe9e0f60c
hlibb <Astashev01@gmail.com>
2022-08-22 18:31:44 +0200

    init

---
+++ src/main/java/com/example/hillelSpring/HillelSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.hillelSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HillelSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HillelSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/hillelSpring/HillelSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hillelSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HillelSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
